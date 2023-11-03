4e9f5f5d53e70e368b96a40668736b8e7020827b
Linus467 <78028034+Linus467@users.noreply.github.com>
2023-11-03 11:14:11 +0000

    Map

--- app/src/main/java/com/griffith/maptrackerproject/Map.kt
+++ app/src/main/java/com/griffith/maptrackerproject/Map.kt
@@ -1,27 +1,106 @@
 package com.griffith.maptrackerproject
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.location.LocationListener
+import android.location.LocationManager
+import android.preference.PreferenceManager
+import android.util.MutableBoolean
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*;
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat.requestLocationUpdates
+import androidx.lifecycle.MutableLiveData
+import org.osmdroid.config.Configuration
+import org.osmdroid.views.MapView
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import java.lang.reflect.Modifier
 
-class Map : ComponentActivity() {
+class Map : ComponentActivity(), LocationListener {
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
+    private val locationUpdateInterval: Long = 5000 // 5 seconds
+    private val locationUpdateDistance: Float = 10f // 10 meters
+
+    private val liveLocations = mutableStateListOf<GeoPoint>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Button(onClick = {
-                val intent = Intent(this,Compass::class.java).apply{
-                    type = "text/plain"
-                    putExtra("ExtraSubject", arrayListOf("student@griffith.ie"))
-                }
-                if(intent.resolveActivity(packageManager) != null){
-                    startActivity(intent)
-                }
-            }) {
-                Text("Go to Compas")
+            Box(){
+                OsmMapView(liveLocations)
+
             }
         }
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+    }
+
+    private fun startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, locationUpdateInterval, locationUpdateDistance, this)
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        val geoPoint = GeoPoint(location.latitude, location.longitude)
+        liveLocations.add(geoPoint)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startLocationUpdates()
+        }
+    }
+
+}
+var followUser : Boolean = false;
+@Composable
+fun OsmMapView(liveLocations: List<GeoPoint>) {
+    val context = LocalContext.current
+    Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context))
+
+    AndroidView(
+        factory = { ctx ->
+            MapView(ctx).apply {
+                setTileSource(TileSourceFactory.MAPNIK)
+                controller.setCenter(GeoPoint(52.5200, 13.4050))
+                controller.setZoom(9.5)
+            }
+        },
+        update = { mapView ->
+            if (liveLocations.isNotEmpty()) {
+                mapView.controller.setCenter(liveLocations.last())
+            }
+        }
+    )
+}
+
+@Composable
+fun FloatingButton() {
+    Button(onClick = {
+        //followUser = !followUser
+    }) {
+        Text("Button Over Map")
     }
 }
